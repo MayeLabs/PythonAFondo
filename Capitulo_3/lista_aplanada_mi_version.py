25ffb758ecdab12e92dd679f325bace2239be4cf
MayeLabs <mayeaguilera96@gmail.com>
2024-12-13 13:13:57 -0500

    Fixed: I corrected the function call.

--- Capitulo_3/lista_aplanada_mi_version.py
+++ Capitulo_3/lista_aplanada_mi_version.py
@@ -1,11 +1,10 @@
 # Ejercicio:
 # Dada una lista de niveles, se debe unificar la lista
-# Entrada [1,20,[2,[3,4,[5]]]] Salida = [,20,2,3,4,5]
+# Entrada [1,20,[2,[3,4,[5]]]] Salida = [1,20,2,3,4,5]
 def aplanar_lista(lst, lst_new):
 
     elem = lst.pop(0)
-    print(elem)
-
+  
     if isinstance(elem, int):
         lst_new.append(elem)
     else:
@@ -17,6 +16,10 @@ def aplanar_lista(lst, lst_new):
 if __name__ == '__main__':
     lst = [1,20,[2,[3,4,[5]]]]
     lst_new = []
-    print(lst)
-    lst_r = aplanar_lista(lst, lst_new)
+    aplanar_lista(lst, lst_new)
     print(lst_new)
+
+    case_2 = [1,[2,3,4,5],[1,[23,[34]],[62]]]
+    lst_c2 = []
+    aplanar_lista(case_2, lst_c2)
+    print(lst_c2)
