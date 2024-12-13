bb97da0e5c81d6883df26701c3aa97ac8ed6408a
MayeLabs <mayeaguilera96@gmail.com>
2024-12-13 12:56:48 -0500

    Practicing recursion with an exercise from the book.

---
+++ Capitulo_3/lista_aplanada_mi_version.py
@@ -0,0 +1,25 @@
+# Ejercicio:
+# Dada una lista de niveles, se debe unificar la lista
+# Entrada [1,20,[2,[3,4,[5]]]] Salida = [,20,2,3,4,5]
+def aplanar_lista(lst, lst_new):
+
+    elem = lst.pop(0)
+    print(elem)
+
+    if isinstance(elem, list):
+        if len(elem) == 1:
+            lst_new.append(elem[0])
+        else:
+            aplanar_lista(elem, lst_new)
+    else:
+        lst_new.append(elem)
+        
+    if len(lst) > 0:
+        aplanar_lista(lst, lst_new)
+
+if __name__ == '__main__':
+    lst = [1,20,[2,[3,4,[5]]]]
+    lst_new = []
+    print(lst)
+    lst_r = aplanar_lista(lst, lst_new)
+    print(lst_new)
