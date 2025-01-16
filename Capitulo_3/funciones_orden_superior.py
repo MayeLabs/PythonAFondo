375d8107547c9a45481c32efa4bedee071db8ffb
MayeLabs <mayeaguilera96@gmail.com>
2025-01-16 07:17:01 -0500

    Hight order function can return functions

--- Capitulo_3/funciones_orden_superior.py
+++ Capitulo_3/funciones_orden_superior.py
@@ -1,6 +1,7 @@
 # Concepto de la programación funcional
-# Funciones que aceptan otras funciones como parametros
-
+# Funciones que pueden:
+# 1. Aceptar otras funciones como parametros
+# 2. Retornar funciones
 
 # Ejemplo: Visualizar como una misma definicion de funciones puede
 # hacer diferentes lógicas usando funciones externas como parametros
@@ -29,6 +30,13 @@ lst_raiz = aplicar_func(range(20), raiz_de_par)
 print(lst_raiz) # [0.0, 0, 1.4142135623730951, 0, 2.0, 0, 2.449489742783178, 0, 2.8284271247461903, 0, 3.1622776601683795, 0, 3.4641016151377544, 0, 3.7416573867739413, 0, 4.0, 0, 4.242640687119285, 0]
 
 # Usando una función lambda
-mul_de_par_8 = lambda x:mul_de_par(x, multi=8)[0, 0, 16, 0, 32, 0, 48, 0, 64, 0, 80, 0, 96, 0, 112, 0, 128, 0, 144, 0]
+mul_de_par_8 = lambda x:mul_de_par(x, multi=8)
 lst_lamb = aplicar_func(range(20), mul_de_par_8)
 print(lst_lamb) # [0, 0, 16, 0, 32, 0, 48, 0, 64, 0, 80, 0, 96, 0, 112, 0, 128, 0, 144, 0]
+
+# Ejemplo 2: Funcion que retorna funciones
+def op(a, b, func):
+    return func(a,b)3510
+
+print(op(45,78,int.__add__)) # 123
+print(op(47, 78, int.__mul__)) # 3666 
